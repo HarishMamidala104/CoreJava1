0a2d4b85c08bd65696fcfc9184d8305a9ff21e4b
Harish Mamidlala <hm22101@corp.ojas-it.com>
2022-06-09 11:05:41 +0530

    second commit

---
+++ PatternJava.java
@@ -0,0 +1,16 @@
+package Harish123;
+
+public class PatternJava {
+
+	public static void main(String[] args) {
+	/*	System.out.println("   J           a        V      V     A       ");
+		System.out.println("   J          a  a       V       v    A  A     ");
+		System.out.println("J  J          a   a       V   V     A    A    ");
+		System.out.println("  J                                          ");*/
+		 System.out.println("   J    a   v     v  a ");
+	        System.out.println("   J   a a   v   v  a a");
+	        System.out.println("J  J  aaaaa   V V  aaaaa");
+	        System.out.println(" JJ  a     a   V  a     a");
+	}
+
+}
